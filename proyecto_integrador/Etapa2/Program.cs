08fc5b1d4119ffe066f6d14f70679dd7cedca780
José Páez <j_m_paez@hotmail.com>
2025-07-04 21:04:04 -0300

    Añadir solución y proyectos para el proyecto integrador
    Se ha creado una solución de Visual Studio con cinco proyectos: Etapa1, Etapa2, Etapa3, Etapa4 y Etapa5. Todos los proyectos utilizan el SDK de .NET y están configurados para .NET 8.0. Se han añadido archivos `Program.cs` con el código correspondiente: Etapa1 incluye un comentario sobre el requerimiento, Etapa2 implementa un formulario básico para la entrada de datos del alumno, y Etapa3, Etapa4 y Etapa5 imprimen "Hello, World!" en la consola.

---
+++ proyecto_integrador/Etapa2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Etapa2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Bienvenidos al proyecto integrador Etapa 2");
+
+			string Nombre;
+			string Apellido;
+			int Edad;
+			string Email;
+
+			string Respuesta;
+
+			// Ingreso de datos del alumno
+			Console.Write("Ingrese su nombre: ");
+			Nombre = Console.ReadLine();
+			Console.Write("Ingrese su apellido: ");
+			Apellido = Console.ReadLine();
+			Console.Write("Ingrese su edad: ");
+			Edad = int.Parse(Console.ReadLine());
+			Console.Write("Ingrese su email: ");
+			Email = Console.ReadLine();
+
+			// Mostrar los datos ingresados
+			Console.WriteLine("<<<< Datos del alumno >>>>");
+			Console.WriteLine($"Nombre: {Nombre}");
+			Console.WriteLine($"Apellido: {Apellido}");
+			Console.WriteLine($"Edad: {Edad}");
+			Console.WriteLine($"Email: {Email}");
+
+			Console.WriteLine("¿Los datos son correctos? - 'S' para confirmar 'N' para cancelar");
+			Respuesta = Console.ReadLine();
+		}
+	}
+}
