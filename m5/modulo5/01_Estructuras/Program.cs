db83e544d7ce21c78a3744d9974d374765e4b014
José Páez <j_m_paez@hotmail.com>
2025-07-18 19:49:54 -0300

    Actualiza proyectos a .NET 8.0 y reescribe código
    Se han realizado cambios significativos en varios archivos de un proyecto de C#. Se añadió la estructura básica en los archivos `.csproj` para compilar aplicaciones de consola en .NET 8.0, incluyendo propiedades como `OutputType`, `TargetFramework`, `RootNamespace`, `ImplicitUsings` y `Nullable`.
    
    El archivo `Program.cs` de `01_Arreglos_Multidimensionales` fue reescrito para incluir la declaración y uso de arreglos multidimensionales, así como la asignación y lectura de valores, impresión de la matriz y búsqueda del mayor valor, con comentarios explicativos.
    
    En `02_Funciones_y_Procedimientos`, se reescribió el código para incluir ejemplos de funciones y procedimientos, y se demostró el uso de parámetros por valor y referencia, añadiendo comentarios para clarificar el propósito de cada sección.
    
    Se creó un nuevo proyecto `01_Estructuras`, que incluye un archivo de proyecto y un `Program.cs` que define estructuras en C#, con ejemplos de declaración y manipulación de arreglos de estructuras.
    
    Finalmente, se actualizó el archivo de solución (`.sln`) para reflejar la nueva estructura del proyecto y los cambios realizados en los archivos de proyecto y código fuente.

---
+++ m5/modulo5/01_Estructuras/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace _01_Estructuras
+{
+	// Estructuras: Son tipos de datos que permiten almacenar diferentes tipos de datos.
+	// Se definen con la palabra reservada struct.
+	// Sintaxis:
+	// struct NombreEstructura
+	// {
+	//     tipoDato1 nombreDato1;
+	//     tipoDato2 nombreDato2;
+	//     ...
+	// }
+	internal class Program
+	{
+		struct Persona
+		{
+			public string Nombre;
+			public string Apellido;
+			string TipoDocumento;
+			public int NumeroDocumento;
+			public DateTime fechaNacimiento;
+			public string Sexo;
+			public string Telefono;
+			public string Domicilio;
+		}
+
+		public struct Cliente
+		{
+			public int Codigo;
+			public string Nombre;
+			public string Apellido;
+			public string CUIT;
+			public string Domicilio;
+		}
+
+		public struct Vehiculo
+		{
+			public string Marca;
+			public string Modelo;
+			public int Año;
+			public double Precio;
+		}
+
+		static void Main(string[] args)
+		{
+			// Declaracion
+			Persona unaPersona;
+			Persona otraPersona;
+
+			// Asignacion de datos
+			unaPersona.Nombre = "Juan";
+			unaPersona.Apellido = "Perez";
+			unaPersona.NumeroDocumento = 50123976;
+			unaPersona.Domicilio = "";
+
+
+			otraPersona.Nombre = "Maria";
+			otraPersona.Apellido = "Sanchez";
+
+			// Mostrar datos
+			Console.WriteLine($" Nombre {unaPersona.Nombre}");
+			Console.WriteLine($" Apellido {unaPersona.Apellido}");
+			Console.WriteLine($" Numero Documento {unaPersona.NumeroDocumento}");
+			Console.WriteLine($" Domicilio {unaPersona.Domicilio}");
+
+			// Declaracion del Arreglo Tipo "Persona"
+			Persona[] personas = new Persona[3];
+
+
+			// Asignacion de valores
+			personas[0].Apellido = "Perez";
+			personas[0].Nombre = "Juan";
+			personas[0].NumeroDocumento = 50123976;
+
+			personas[1].Apellido = "Sanchez";
+			personas[1].Nombre = "Maria";
+			personas[1].NumeroDocumento = 12345678;
+
+			personas[2].Apellido = "Rodriguez";
+			personas[2].Nombre = "Alberto";
+			personas[2].NumeroDocumento = 45123987;
+
+
+			// Mostrar datos
+			Console.WriteLine($" Nombre: {personas[2].Nombre}");
+			Console.WriteLine($" Apellido: {personas[2].Apellido}");
+			Console.WriteLine($" Numero Documento: {personas[2].NumeroDocumento}");
+
+			Console.WriteLine("-- Datos en el Arreglo Personas--");
+			// Mostrar datos en el arreglo de Personas
+			for (int i = 0; i < personas.Length; i++)
+			{
+				Console.WriteLine($" Nombre: {personas[i].Nombre}");
+				Console.WriteLine($" Apellido: {personas[i].Apellido}");
+				Console.WriteLine($" Numero Documento: {personas[i].NumeroDocumento}");
+			}
+
+
+			Console.WriteLine("-- Datos en el Arreglo Vehiculos --");
+			Vehiculo[] vehiculos = new Vehiculo[3];
+
+			vehiculos[0].Marca = "Toyota";
+			vehiculos[0].Modelo = "Corolla";
+			vehiculos[0].Año = 2020;
+			vehiculos[0].Precio = 20000;
+
+			vehiculos[1].Marca = "Ford";
+			vehiculos[1].Modelo = "Fiesta";
+			vehiculos[1].Año = 2019;
+			vehiculos[1].Precio = 15000;
+
+			vehiculos[2].Marca = "Honda";
+			vehiculos[2].Modelo = "Civic";
+			vehiculos[2].Año = 2021;
+			vehiculos[2].Precio = 25000;
+
+			for (int i = 0; i < vehiculos.Length; i++)
+			{
+				Console.WriteLine($"Marca: {vehiculos[i].Marca}, Modelo: {vehiculos[i].Modelo}, Año: {vehiculos[i].Año}, Precio: {vehiculos[i].Precio}");
+			}
+		}
+	}
+}
